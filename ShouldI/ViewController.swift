433a0bdba06dbe9b0abe632d214015103b96a501
luisgnzlz <luisgnzlz33@gmail.com>
2024-12-13 08:49:28 -0800

    Initial Commit

---
+++ ShouldI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShouldI
+//
+//  Created by Luis Gonzalez on 12/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShouldITests/ShouldITests.swift
@@ -0,0 +1,17 @@
+//
+//  ShouldITests.swift
+//  ShouldITests
+//
+//  Created by Luis Gonzalez on 12/13/24.
+//
+
+import Testing
+@testable import ShouldI
+
+struct ShouldITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
